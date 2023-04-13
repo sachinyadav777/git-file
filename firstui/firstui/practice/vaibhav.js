9db1fc9f3237e6f4bfdb8d493a805e45aa648fa2
sachin <sy396610@gmail.com>
2023-04-14 01:56:17 +0530

    changes

---
+++ firstui/firstui/practice/vaibhav.js
@@ -0,0 +1,31 @@
+let a =5;
+
+
+function b(){
+   let a = 8;
+    console.log(a);
+}
+ function x(){
+     console.log(a);
+ }
+
+
+ x();
+ b();
+ console.log(a);
+ 
+
+
+// sachin yadav
+
+// 5
+// 8
+// 3
+// 5/3
+
+// vaibhav
+
+// 5
+// 5
+// 8
+// 3
